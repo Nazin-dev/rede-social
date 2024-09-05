b75d6db9ecec0dcf0962ef2f637fa7363f9e8fab
Nailton Reis <nazin.dev@gmail.com>
2024-09-05 00:11:16 -0300

    Layout da HomePage feito, falta os ajustes

---
+++ src/assets/components/PageHomeComponents/PostItem.jsx
@@ -0,0 +1,31 @@
+import '../PageHomeComponents/PostItem.css';
+import Profile from '../../img/profile.png'
+
+function PostItem({userName, userLocation, postText, postImage, likes}) {
+  return (
+    <div className="post">
+      <div className="post-header">
+        <div className="profile-info">
+          <img src={Profile} alt="" className="profile-pic" />
+          <div className="post-info">
+            <span className="username">{userName}</span>
+            <span className="location">{userLocation}</span>
+          </div>
+        </div>
+        <div className="options-profile"><i className="bi bi-three-dots"></i></div>
+      </div>
+      <p className="post-text">{postText}</p>
+      {postImage && <img src={postImage} alt="Post" className="post-image" />}
+      <div className="icons-post">
+        <div className="icon like"><i className="bi bi-heart"></i></div>
+        <div className="icon comments"><i className="bi bi-chat"></i></div>
+        <div className="icon send-post"><i className="bi bi-send"></i></div>
+      </div>
+      <div className="post-footer">
+        <span>{likes} likes</span>
+      </div>
+    </div>
+  );
+};
+
+export default PostItem;
--- src/assets/pages/HomePage.jsx
+++ src/assets/pages/HomePage.jsx
@@ -1,11 +1,14 @@
 import '../pages/HomePage.css'
 import Navbar from '../components/PageHomeComponents/Navbar.jsx';
+import PostItem from '../components/PageHomeComponents/PostItem.jsx';
 import BottomNavigation from '../components/PageHomeComponents/BottomNavigation.jsx';
+import ImagePost from '.././img/post.png'
 
 function HomePage() {
   return (
     <>
     <Navbar />
+    <PostItem userName="Henry" userLocation="Paraguai" postText="Earlier this year, a friend of mine almost had his young daughter taken from him in California just because he wanted her to wait a few years to permanently transition." postImage={ImagePost} likes="567"/>
     <BottomNavigation />
     </>
   );
