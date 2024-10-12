e63129bea50e62020f1478bf8a8346d414cb50ed
Dann Luciano <dannluciano@gmail.com>
2024-10-11 22:33:37 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./assets/pages/Login";
 import CreateAccount from "./assets/pages/CreateAccount";
 import HomePage from "./assets/pages/HomePage";
